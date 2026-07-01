5f644040b92b6a325dd3fb1edad29bb0eda75bdc
HookCraft <hookcraft@users.noreply.github.com>
2026-07-01 19:40:10 +0300

    chore: remove deprecated apps/api to fix build conflicts

--- apps/api/src/drafts/drafts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DraftsService } from './drafts.service';
-import { DraftsController } from './drafts.controller';
-
-@Module({
-  providers: [DraftsService],
-  controllers: [DraftsController],
-})
-export class DraftsModule {}
